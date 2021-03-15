4664f6f884ecc09b1197ab0469ec1589f47b8dff
EstebanP <epinczinger@gmail.com>
2021-03-15 17:27:12 -0300

    fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Big from 'big.js';
+import PropTypes from 'prop-types';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const Items = props => {
+  const { total } = props;
+  return (
+    <h1 >
+      Number:
+      {total}
+    </h1>
+  );
+};
+
+Items.propTypes = {
+  total: PropTypes.number,
+};
+
+Items.defaultProps = {
+  total: 0,
+};
+
+export default class App extends React.PureComponent {
+  render() {
+    const a = Big(1.2).mul(2);
+    return <Items total={`${a}`} />;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
