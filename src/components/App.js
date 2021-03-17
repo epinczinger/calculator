8a2a2c5c0349b8a6d79a25ee7249fc6ae528b99a
EstebanP <epinczinger@gmail.com>
2021-03-17 13:39:21 -0300

    add button and buttonpanel components

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import ButtonPanel from './ButtonPanel'
+import Display from './Display'
 
 export default function App() {
     return (
---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Button from './Button'
+
+export default function ButtonPanel() {
+  return (
+    <div>
+      <div className="button-row">
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+      </div>
+      <div className="button-row">
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+      </div>
+      <div className="button-row">
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+      </div>
+      <div className="button-row">
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+      </div>
+      <div className="button-row">
+        <Button />
+        <Button />
+        <Button />
+      </div>
+    </div>
+  );
+}
