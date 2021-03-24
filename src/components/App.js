6b99845fa861772785cd1c152e6827a61980b729
EstebanP <epinczinger@gmail.com>
2021-03-24 20:45:40 -0300

    display and button panel styled

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,10 @@ import Display from './Display';
 export default function App() {
   return (
     <>
-      <Display />
-      <ButtonPanel />
+      <div className="flex-column items-center bg-gray-800 justify-center h-4/5 sm:h-2/3 w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 m-auto">
+        <Display />
+        <ButtonPanel />
+      </div>
     </>
   );
 }
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,35 +3,87 @@ import Button from './Button';
 
 export default function ButtonPanel() {
   return (
-    <div>
-      <div className="button-row">
-        <Button name="AC" />
-        <Button name="+/-" />
-        <Button name="%" />
-        <Button name="÷" />
+    <div className="bg-gray-800 text-xl">
+      <div className="flex">
+        <div className="bg-blue-600 w-1/4 p-5 flex justify-center border rounded border-gray-800 ">
+          <Button className="m-2" name="AC" />
+        </div>
+        {' '}
+        <div className="bg-blue-600 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button n className="m-2" name="+/-" />
+        </div>
+        {' '}
+        <div className="bg-blue-600 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="%" />
+        </div>
+        {' '}
+        <div className="bg-yellow-500 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="÷" />
+        </div>
       </div>
-      <div className="button-row">
-        <Button name="7" />
-        <Button name="8" />
-        <Button name="9" />
-        <Button name="X" />
+      <div className="flex">
+        <div className="bg-green-700 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="7" />
+        </div>
+        {' '}
+        <div className="bg-green-700 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="8" />
+        </div>
+        {' '}
+        <div className="bg-green-700 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="9" />
+        </div>
+        {' '}
+        <div className="bg-yellow-500 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="X" />
+        </div>
       </div>
-      <div className="button-row">
-        <Button name="4" />
-        <Button name="5" />
-        <Button name="6" />
-        <Button name="-" />
+      <div className="flex">
+        <div className="bg-green-700 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="4" />
+        </div>
+        {' '}
+        <div className="bg-green-700 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="5" />
+        </div>
+        {' '}
+        <div className="bg-green-700 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="6" />
+        </div>
+        {' '}
+        <div className="bg-yellow-500 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="-" />
+        </div>
       </div>
-      <div className="button-row">
-        <Button name="1" />
-        <Button name="2" />
-        <Button name="3" />
-        <Button name="+" />
+      <div className="flex">
+        <div className="bg-green-700 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="1" />
+        </div>
+        {' '}
+        <div className="bg-green-700 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="2" />
+        </div>
+        {' '}
+        <div className="bg-green-700 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="3" />
+        </div>
+        {' '}
+        <div className="bg-yellow-500 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="+" />
+        </div>
       </div>
-      <div className="button-row">
-        <Button name="0" />
-        <Button name="." />
-        <Button name="=" />
+      <div className="flex">
+        <div className="bg-green-700 w-1/2 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="0" />
+        </div>
+        {' '}
+        <div className="bg-green-700 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="." />
+        </div>
+        {' '}
+        <div className="bg-yellow-500 w-1/4 p-5 flex justify-center border rounded border-gray-800">
+          <Button className="m-2" name="=" />
+        </div>
       </div>
     </div>
   );
