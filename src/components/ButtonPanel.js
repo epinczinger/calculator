6374319940a20f1409392f462636ac3caf57c2aa
EstebanP <epinczinger@gmail.com>
2021-03-25 11:56:10 -0300

    modify margin on button and delete extra div

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -6,83 +6,83 @@ export default function ButtonPanel() {
     <div className="bg-gray-800 text-xl">
       <div className="flex">
         <div className="bg-blue-600 hover:bg-blue-700 w-1/4 p-5 flex justify-center border rounded border-gray-800 ">
-          <Button className="m-2" name="AC" />
+          <Button name="AC" />
         </div>
         {' '}
         <div className="bg-blue-600 hover:bg-blue-700 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button n className="m-2" name="+/-" />
+          <Button name="+/-" />
         </div>
         {' '}
         <div className="bg-blue-600 hover:bg-blue-700 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="%" />
+          <Button name="%" />
         </div>
         {' '}
         <div className="bg-yellow-500 hover:bg-yellow-600 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="÷" />
+          <Button name="÷" />
         </div>
       </div>
       <div className="flex">
         <div className="bg-green-700 hover:bg-green-800 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="7" />
+          <Button name="7" />
         </div>
         {' '}
         <div className="bg-green-700 hover:bg-green-800 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="8" />
+          <Button name="8" />
         </div>
         {' '}
         <div className="bg-green-700 hover:bg-green-800 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="9" />
+          <Button name="9" />
         </div>
         {' '}
         <div className="bg-yellow-500 hover:bg-yellow-600 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="X" />
+          <Button name="X" />
         </div>
       </div>
       <div className="flex">
         <div className="bg-green-700 hover:bg-green-800 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="4" />
+          <Button name="4" />
         </div>
         {' '}
         <div className="bg-green-700 hover:bg-green-800 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="5" />
+          <Button name="5" />
         </div>
         {' '}
         <div className="bg-green-700 hover:bg-green-800 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="6" />
+          <Button name="6" />
         </div>
         {' '}
         <div className="bg-yellow-500 hover:bg-yellow-600 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="-" />
+          <Button name="-" />
         </div>
       </div>
       <div className="flex">
         <div className="bg-green-700 hover:bg-green-800 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="1" />
+          <Button name="1" />
         </div>
         {' '}
         <div className="bg-green-700 hover:bg-green-800 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="2" />
+          <Button name="2" />
         </div>
         {' '}
         <div className="bg-green-700 hover:bg-green-800 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="3" />
+          <Button name="3" />
         </div>
         {' '}
         <div className="bg-yellow-500 hover:bg-yellow-600 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="+" />
+          <Button name="+" />
         </div>
       </div>
       <div className="flex">
         <div className="bg-green-700 hover:bg-green-800 w-1/2 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="0" />
+          <Button name="0" />
         </div>
         {' '}
         <div className="bg-green-700 hover:bg-green-800 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="." />
+          <Button name="." />
         </div>
         {' '}
         <div className="bg-yellow-500 hover:bg-yellow-600 w-1/4 p-5 flex justify-center border rounded border-gray-800">
-          <Button className="m-2" name="=" />
+          <Button name="=" />
         </div>
       </div>
     </div>
