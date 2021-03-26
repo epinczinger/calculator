f8b3a3af2eb7fc277da9ad0876ee64e5d684f00b
EstebanP <epinczinger@gmail.com>
2021-03-25 22:19:56 -0300

    change size of display

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export default function Display(props) {
   const { result } = props;
   return (
-    <div className="flex justify-end bg-gray-400 border-4 border-gray-800 rounded-md text-4xl p-2">
+    <div className="flex justify-end bg-gray-400 border-4 border-gray-800 rounded-md text-2xl lg-text-3xl p-2">
       <h2>{result}</h2>
     </div>
   );
