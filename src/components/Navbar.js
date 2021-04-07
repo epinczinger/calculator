ea14694af74e3ad8a31396fd8df36227048cff67
EstebanP <epinczinger@gmail.com>
2021-04-07 11:17:59 -0300

    style navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,22 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div className="flex justify-around">
-      <div className="flex justify-start">
-        <h2>Math-Magicians</h2>
+    <div className="flex justify-around text-lg bg-gray-700 text-gray-200 ">
+      <div className="flex justify-start hover:text-gray">
+        <div className=" px-3 py-2 hover:bg-gray-600">
+          <Link to="/">Math-Magicians </Link>
+        </div>
       </div>
-      <div className="flex justify-end">
-        <Link to="/">Home </Link>
-        <Link to="/calculator">Calculator </Link>
-        <Link to="/quote">Quote </Link>
+      <div className="flex justify-end ">
+        <div className=" px-3 py-2 hover:bg-gray-600 ">
+          <Link to="/">Home </Link>
+        </div>
+        <div className=" px-3 py-2 hover:bg-gray-600">
+          <Link to="/calculator">Calculator </Link>
+        </div>
+        <div className=" px-3 py-2 hover:bg-gray-600">
+          <Link to="/quote">Quote </Link>
+        </div>
       </div>
     </div>
   );
