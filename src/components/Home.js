bfab5a83a429aa79c81bbde804d73a3be207af3c
EstebanP <epinczinger@gmail.com>
2021-04-10 17:00:33 -0300

    final style on change and quote section

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to our Page</h1>
-      <p>Lorem impsum something more</p>
+    <div className="text-center px-2 text-gray-800">
+      <h1 className="py-4 text-lg font-bold">Welcome to Math-magicians!</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
     </div>
   );
 }
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 function Quote() {
   return (
-    <div className="bg-calculator bg-center h-full text-gray-800 font-bold flex flex-col justify-center">
-      <h3 className="text-4xl text-center pb-8 justify-self-start">
+    <div className="text-gray-800 font-bold flex flex-col justify-center p-2">
+      <h3 className="text-xl text-center p-4 justify-self-start">
         Quote of the day!
       </h3>
-      <h1 className="text-xl text-center p-4">
-        Mathematics is not about numbers, equations, computations, or
-        algorithms: it is about understanding. –William Paul Thurston
+      <h1 className="text-lg text-center p-4">
+        <q>
+          Mathematics is not about numbers, equations, computations, or
+          algorithms: it is about understanding.
+        </q>
+        <br />
+        <cite className="text-sm">William Paul Thurston</cite>
       </h1>
     </div>
   );
